b43fcd722dd0d9287e01455145875f0b5bc74cd2
fpsfitri <fpsfitri@gmail.com>
2016-07-13 23:21:14 +0900

    content value

--- app/src/main/java/ajou/walteg/WaltegDatabaseHelper.java
+++ app/src/main/java/ajou/walteg/WaltegDatabaseHelper.java
@@ -19,13 +19,12 @@ public class WaltegDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
         db.execSQL("CREATE TABLE purchase (idpurchase INTEGER PRIMARY KEY AUTO INCREMENT, date STRING, itemid INTEGER, totalnumber INTEGER, totalprice INTEGER);");
         db.execSQL("CREATE TABLE inventory (idinventory INTEGER PRIMARY KEY AUTO INCREMENT,nameinventory STRING , dateexpired STRING, total INTEGER);");
         db.execSQL("CREATE TABLE menu (idmenu INTEGER PRIMARY KEY AUTO INCREMENT,namemenu STRING , price INTEGER);");
         db.execSQL("CREATE TABLE cooking (idcooking INTEGER PRIMARY KEY AUTO INCREMENT,date STRING);");
-        db.execSQL("CREATE TABLE usage (idcooking INTEGER,idinventory INTEGER,totalusage INTEGER);");
-        db.execSQL("CREATE TABLE sold (idcooking INTEGER,idmenu INTEGER,totalsell INTEGER, excessmenu INTEGER);");
+        db.execSQL("CREATE TABLE usage (idusage INTEGER PRIMARY KEY AUTO INCREMENT, idcooking INTEGER,idinventory INTEGER,totalusage INTEGER);");
+        db.execSQL("CREATE TABLE sold (idsold INTEGER PRIMARY KEY AUTO INCREMENT, idcooking INTEGER,idmenu INTEGER,totalsell INTEGER, excessmenu INTEGER);");
 
     }
 
@@ -59,23 +58,34 @@ public class WaltegDatabaseHelper extends SQLiteOpenHelper {
     }
 
     public void addInventory(Inventory i) {
-
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cInventory = i.getContentValues();
+        db.insert("inventory", null, cInventory);
     }
 
     public void addMenu(Menu m){
-
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cMenu = m.getContentValues();
+        db.insert("menu", null, cMenu);
     }
+
     public void addCooking(Cooking c){
-
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cCooking = c.getContentValues();
+        db.insert("cooking", null, cCooking);
     }
+
     public void addUsage(Usage u){
-
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cUsage = u.getContentValues();
+        db.insert("usage", null, cUsage);
     }
+
     public void addSold(Sold s){
-
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cSold = s.getContentValues();
+        db.insert("sold", null, cSold);
     }
-
-
 }
 
 class Purchase {
@@ -112,6 +122,13 @@ class Inventory{
         this.dateexpired=dateexpired;
         this.total=total;
     }
+    public ContentValues getContentValues(){
+        ContentValues cInventory = new ContentValues();
+        cInventory.put("nameinventory", nameinventory);
+        cInventory.put("dateexpired", dateexpired);
+        cInventory.put("total", total);
+        return cInventory;
+    }
 }
 
 class Menu{
@@ -123,6 +140,12 @@ class Menu{
         this.namemenu=namemenu;
         this.price=price;
     }
+    public ContentValues getContentValues(){
+        ContentValues cMenu = new ContentValues();
+        cMenu.put("namemenu", namemenu);
+        cMenu.put("price", price);
+        return cMenu;
+    }
 }
 
 class Cooking{
@@ -132,29 +155,53 @@ class Cooking{
         this.idcooking=idcooking;
         this.date=date;
     }
+    public ContentValues getContentValues(){
+        ContentValues cCooking = new ContentValues();
+        cCooking.put("date", date);
+        return cCooking;
+    }
 }
 
 class Usage{
+    int idusage;
     int idcooking;
     int idinventory;
     int totalusage;
-    public Usage(int idcooking, int idinventory, int totalusage){
+    public Usage(int idusage, int idcooking, int idinventory, int totalusage){
+        this.idusage=idusage;
         this.idcooking=idcooking;
         this.idinventory=idinventory;
         this.totalusage=totalusage;
     }
+    public ContentValues getContentValues(){
+        ContentValues cUsage = new ContentValues();
+        cUsage.put("idcooking", idcooking);
+        cUsage.put("idinventory", idinventory);
+        cUsage.put("totalusage", totalusage);
+        return cUsage;
+    }
 }
 
 class Sold{
+    int idsold;
     int idcooking;
     int idmenu;
     int totalsell;
     int excessmenu;
-    public Sold(int idcooking, int idmenu, int totalsell, int excessmenu){
+    public Sold(int idsold, int idcooking, int idmenu, int totalsell, int excessmenu){
+        this.idsold=idsold;
         this.idcooking=idcooking;
         this.idmenu=idmenu;
         this.totalsell=totalsell;
         this.excessmenu=excessmenu;
+    }
+    public ContentValues getContentValues(){
+        ContentValues cUsage = new ContentValues();
+        cUsage.put("idcooking", idcooking);
+        cUsage.put("idmenu", idmenu);
+        cUsage.put("totalsell", totalsell);
+        cUsage.put("excessmenu", excessmenu);
+        return cUsage;
     }
 }
 
